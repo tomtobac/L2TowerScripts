6a4b35db674c5878bbd43deea5e3561176c2340d
hkares <somethingofhappy@gmail.com>
2015-11-09 18:55:26 +0100

    Create arrangedTargeting.lua

---
+++ arrangedTargeting.lua
@@ -0,0 +1,22 @@
+
+function arrangedMobListbyDistance(distance)
+    local array = {};
+    local moblist = GetMonsterList();
+    local index = 0;
+    for mob in moblist.list do
+        if (distance>mob:GetDistance() and mob:GetHp()>0 and  not mob:IsAlikeDeath()) then
+            array[index] = mob;
+            index = index + 1;
+        end;
+    end;
+        local sort_func = function( a,b ) return a.GetDistance() < b.GetDistance() end
+        table.sort( array, sort_func )
+    return array;
+end;
+
+local var = arrangedMobListbyDistance(2000)
+local index = 0;
+ShowToClient("", "List Mobs: ");
+for mob in var.list do
+ShowToClient(tostring(index), "Name: " .. tostring(mob:GetName()) .. " - distance: " .. tostring(mob:GetDistance()));
+end;
