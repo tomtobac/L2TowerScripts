e1d8076594a2daec6dd51a12b722b0daafff8ed5
hkares <somethingofhappy@gmail.com>
2015-11-09 20:26:13 +0100

    Update arrangedTargeting.lua

--- arrangedTargeting.lua
+++ arrangedTargeting.lua
@@ -3,24 +3,23 @@ function arrangedMobListbyDistance(distance)
     local moblist = GetMonsterList();
     local index = 0;
     for mob in moblist.list do
-        if (distance>mob:GetDistance() and mob:GetHp()>0 and  not mob:IsAlikeDeath()) then
+        if (distance<mob:GetDistance() and mob:GetHp()>0 and  not mob:IsAlikeDeath()) then
             array[index] = mob;
             index = index + 1;
         end;
     end;
-        local sort_func = function( a,b ) return a.GetDistance() < b.GetDistance() end
-        table.sort( array, sort_func )
+        --local sort_func = function( a,b ) return a.GetDistance() < b.GetDistance() end
+        --table.sort( array, sort_func )
     return array;
 end;
 
-local var = arrangedMobListbyDistance(2000)
+local var = arrangedMobListbyDistance(2000);
 local index = 0;
 ShowToClient("", "List Mobs: ");
-if (var ~= nil) then
+if (var == nil) then
 	ShowToClient("", "No mobs arround.");
 else
-	for mob in var.list do
-		ShowToClient(tostring(index), "Name: " .. tostring(mob:GetName()) .. " - distance: " .. tostring(mob:GetDistance()));
-		index = index + 1;
+	for index = 0, table.getn(var) do
+	ShowToClient(tostring(index), "Name: " .. tostring(var[index]:GetName()) .. " - distance: " .. tostring(var[index]:GetDistance()));
 	end;
 end;
