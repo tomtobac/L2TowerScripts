4f984ef58f96fa34b3fce3f4ed02e5d46d17e880
hkares <somethingofhappy@gmail.com>
2015-11-09 23:47:52 +0100

    Update targeting.lua

--- targeting.lua
+++ targeting.lua
@@ -28,29 +28,40 @@ function summonGivesMana()
 	end;
 end;
 
+---------------------- FUNCTION : DONT MOVE ---------------------
+
+function checkSpot(location)
+	SpotLocation = nil;
+	SpotLocation = location;
+	if (GetDistanceVector(GetMe():GetLocation(),SpotLocation) > 60) and (GetDistanceVector(SpotLocation,GetMe():GetLocation()) < 2500) then
+		MoveToNoWait(SpotLocation);
+    end;
+end;
 
 ----------------------- SCRIPT ---------------------------------
 
 
 local old_id = 0; -- Doesn't matter.
 local range = 2500; -- Range 
+local location = GetMe():GetLocation();
 --local id_skill = GetSkillIdByName("Aqua Swirl");
 repeat
 target=targetMobs(range)
     if (target~=nil and target:GetHp()>0 and not target:IsAlikeDeath() and (target:GetId()~=old_id) ) then  
-        ShowToClient("BOT",  tostring("NEW TARGET: "..target:GetName().." - id: " .. target:GetId()));
+        --ShowToClient("BOT",  tostring("NEW TARGET: "..target:GetName().." - id: " .. target:GetId()));
         Command("/target "..tostring(target:GetName()));
         old_id = target:GetId(); -- Get new ID.
 		
         repeat -- Waitting for the dead of target.
-		Sleep(1000);
-		summonGivesMana();
-		--UseSkillRaw(id_skill, false, false);
-		Command("/useshortcut 1 1")
-	until (target:IsAlikeDeath()); -- Until the mob is dead.
+            Sleep(1000);
+			summonGivesMana();
+			--UseSkillRaw(id_skill, false, false);
+			Command("/useshortcut 1 1")
+        until (target:IsAlikeDeath() or GetTarget() == nil); -- Until the mob is dead.
 
         CancelTarget(true); -- Cancel current Target (ESC).
-        ShowToClient("BOT", tostring(target:GetName()).." is dead.");
+        --ShowToClient("BOT", tostring(target:GetName()).." is dead.");
     end;
 Sleep(1000);
+checkSpot(location);
 until false
