d13d32545e7547f2262139d1bf7a56f5b0bfda0a
hkares <somethingofhappy@gmail.com>
2015-11-11 00:04:07 +0100

    Update info.lua

--- info.lua
+++ info.lua
@@ -1,19 +1,25 @@
 --ShowToClient("", tostring(GetTarget():GetId()));
 --ShowToClient("", tostring(GetMe():GetMp()));
 
-mostraMobs()
-
 function mostraMobs()
-	local moblist = GetMonsterList();
+	local moblist = GetMonsterList()
 	for mob in moblist.list do
-	ShowToClient("mob", tostring(mob:GetId()))
+		ShowToClient("mob", tostring(mob:GetName()) .. tostring(mob:GetNpcId()))
 	end
 end
 
 function mostraSkills()
-	local var = GetSkills();
-	
+	local var = GetSkills()
 	for skill in var.list do
 	ShowToClient("BOT", "Skill name: " .. tostring(skill.name) .. " ID skill: " .. tostring(skill.skillId));
-	end;
+	end
 end
+
+--mostraMobs()
+ShowToClient("Bot", "asdas")
+TargetNpc("Plain Grizzly", 21097)
+Target(21097)
+TargetRaw(21097)
+Target("Plain Grizzly")
+--Target(21098)
+--Target(21099)
