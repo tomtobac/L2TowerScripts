2358aa8db1790ab94bdd79cbeb559cc017019562
matiesmengo <matiesmengo@gmail.com>
2015-11-11 10:14:09 +0100

    change a condicional
    Condicional line 7 change '<' for '>'.
    
    if the distance of the player is more than ur save distance, check like true.
    if player is party member, check like true.
    if player is in whitelist, check like true.

--- Alarm.lua
+++ Alarm.lua
@@ -4,25 +4,26 @@ function checkPlayers(save_distance, white_list)
     local unlisted_players = "UNLISTED PLAYERS \n ";
     for player in playerlist.list do
         local check = false;
-        if (player:GetDistance() < save_distance) then
-        check = true; -- We don't care about him, its far away.
+        if (player:GetDistance() > save_distance) then 
+            check = true; -- We don't care about him, its far away.
         end;
-            -------- CHECK IF THE PLAYER IS IN THE PARTY MEMBER LIST --------
-            for party in party_members.list do
-                if (player = party) then
+        -------- CHECK IF THE PLAYER IS IN THE PARTY MEMBER LIST --------
+        for party in party_members.list do
+            if (player = party) then
                 check = true; -- he is a party member.
-                end;
             end;
-            -------- CHECK IF THE PLAYER IS IN THE WHITE_LIST --------
-            for white in white_list.list do
-                if (player = white) then
+        end;
+        -------- CHECK IF THE PLAYER IS IN THE WHITE_LIST --------
+        for white in white_list.list do
+            if (player = white) then
                 check = true; -- he is in white list member.
-                end;
             end;
-            if (not check) then
+        end;
+    
+        if (not check) then
             distance = player:GetRangeTo(GetMe());
             unlisted_players = unlisted_players .. player:GetName() .. " at " .. distance/100 .. "\n";
-            end;
+        end;
     end;
     return false;
 end;
