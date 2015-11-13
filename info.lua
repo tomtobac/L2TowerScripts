244560278a3b3a7e6b0f8a98642d1219a3cea4b0
hkares <somethingofhappy@gmail.com>
2015-11-13 14:08:45 +0100

    Update info.lua

--- info.lua
+++ info.lua
@@ -2,10 +2,26 @@
 --ShowToClient("", tostring(GetMe():GetMp()));
 
 function mostraMobs()
-	local moblist = GetMonsterList()
+	local moblist = GetNpcList()
 	for mob in moblist.list do
-		ShowToClient("mob", tostring(mob:GetName()) .. tostring(mob:GetNpcId()))
+		--	ShowToClient("mob", tostring(mob:GetName()) .. " " .. tostring(mob:GetNpcId()))
+		Sleep(500)
+		--	ShowToClient("bot", "target mob:npc id")
+		Target(mob:GetNpcId())
+		--ShowToClient("bot", "target mob:get id")
+		--Target(mob:GetId())
+		--ShowToClient("bot", "target mob")
+		--Target(mob)
+		--	ShowToClient("bot", "targetRaw mob:get id")
+		TargetRaw(mob:GetId())
+		--	ShowToClient("bot", "targetRaw mob:npc id")
+		TargetRaw(mob:GetNpcId())
+		--ShowToClient("bot", "targetNpc mob:get name, mob:npc id")
+		--TargetNpc(mob:GetName(), mob:GetNpcId())
+		--ShowToClient("bot", "targetNpc mob:get name, mob:get id")
+		--TargetNpc(mob:GetName(), mob:GetId())
 	end
+	ShowToClient("bot", "done")
 end
 
 function mostraSkills()
@@ -15,11 +31,28 @@ function mostraSkills()
 	end
 end
 
+function mostraItems()
+	local var = ItemDataList()
+	--local var = var:GetList()
+	if(var == nil) then
+	ShowToclient("bot", "nothing to show")
+	else
+		for obj in var.list do
+		ShowToClient("obj", "hola")
+		end
+	end
+end
+	
+
 --mostraMobs()
-ShowToClient("Bot", "asdas")
+ShowToClient("Bot", "otra")
+--mostraSkills()
+mostraItems()
+--[[
 TargetNpc("Plain Grizzly", 21097)
 Target(21097)
 TargetRaw(21097)
 Target("Plain Grizzly")
 --Target(21098)
 --Target(21099)
+--]]
