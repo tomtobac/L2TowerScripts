933ad38be9829a93f4f8bda1579799157d02ed20
matiesmengo <matiesmengo@gmail.com>
2015-11-13 08:25:48 +0100

    Commant(nil) founded
    solved, but commit without test

--- targeting.lua
+++ targeting.lua
@@ -2,54 +2,54 @@
 
 --Global variables don't need declaration
 location = GetMe():GetLocation()		-- Location of the main spot.
-range = 2500; 					-- Range of targeting
+range = 20; 					-- Range of targeting
 overhit_damage = 200; 				-- Overhit damage
 totalmana = 484;				-- Max pool of ur mana character
 perce_mana_summon = 75; 			-- Summon restore mana at Percentage
 perce_mana_sit = 40;				-- Percentage to sit
 perce_mana_stand = 80;				-- Percentage to stand
+perce_hp_heal = 75;
 restoring_mp = false				-- Are we restoring mp?
 
 --Bar controls:
 mainNuke = "/useshortcut 1 1";
 overHitNuke = "/useshortcut 1 2";
 summonRestoreMp = "/useshortcut 1 3";
-
+skillHeal ="/useshortcut 1 5";
 ----------------------- FUNCTION : TARGETMOBS ---------------------------------
 function targetMobs(range)
 local moblist = GetMonsterList();
 local currentrange=range;
-local list = " \n ";
-    
-    for mob in moblist.list do
-            list = list .. tostring(mob:GetName()) .. " \n ";
-        if (currentrange>mob:GetDistance() and mob:GetHp()>0 and  not mob:IsAlikeDeath()) then
-            currentrange=mob:GetDistance(); -- Looking for the nearest mob.
-            currentmob=mob;
-        end;
-    end;
+local currentmob = nil
 
+	for mob in moblist.list do
+		local maxRange = GetDistanceVector(location,GetMe():GetLocation()) + mob:GetDistance();
+        	if (currentrange > maxRange and mob:GetHp()>0 and  not mob:IsAlikeDeath()) then
+        		currentrange=mob:GetDistance(); -- Looking for the nearest mob.
+            		currentmob=mob;
+        	end;
+    	end;
     return currentmob;
 end;
 
 ----------------------- FUNCTION : SUMMON GIVES MANA ---------------------------------
 
 function summonGivesMana()
-	if(GetMe():GetMp() < (totalmana * perce_mana_summon / 100)) then
-	Command(summonRestoreMp); -- Summon uses recharge
+	if(GetMe():GetMp() < (totalmana * perce_mana_summon / 100)) then		
+		Command(summonRestoreMp); -- Summon uses recharge
 	end;
 end;
 
 ---------------------- FUNCTION : DONT MOVE ---------------------
 
 function checkSpot(location)
-	SpotLocation = location;
-	if (GetDistanceVector(GetMe():GetLocation(),SpotLocation) > 60) and (GetDistanceVector(SpotLocation,GetMe():GetLocation()) < 2500) then
-		Command("/pickup")
-		Sleep(1000)
-		MoveToNoWait(SpotLocation)
-		Sleep(750)
-    	end
+	selfHeal()
+	Command("/pickup")
+	Sleep(1000)
+	Command("/pickup")
+	Sleep(1000)
+	MoveToNoWait(location)
+		
     	checkMana() -- call 'check mana' after it moves.
 end
 
@@ -62,14 +62,17 @@ function checkMana()
 	elseif (GetMe():GetMp() > (totalmana * perce_mana_stand / 100) and GetMe():IsSiting() and restoring_mp) then -- Mana over 80% ~
 		Command("/stand");
 		restoring_mp = false
-	end
+	elseif (restoring_mp or GetTarget()~=nil) then
+		Sleep(1000)
+		Command("/pickup")
+	end --Si hi ha un target q te pega.... hem de fer algp
 end
 
 ---------------------- Function: useFightSkills -----------------------
 
 
 function useFightSkills()
-	if (GetTarget()~=nil) then
+	if (GetTarget()~=nil) then	
 		if (GetTarget():GetHp()< overhit_damage and not GetTarget():IsAlikeDeath()) then
 			Command(overHitNuke) 	--OverhitNuke
 		else
@@ -78,14 +81,6 @@ function useFightSkills()
 	end
 end
 
------------------------ FUNCTION : imDeath ---------------------------------
-
-function imDeath()
-	if (GetMe():IsAlikeDeath()) then
-	    SetPause(true)
-	    LogOut()
-	end
-end
 
 ----------------------- FUNCTION : TARGET MOB  ---------------------------------
 
@@ -100,32 +95,33 @@ local target=targetMobs(range)
 			end
 		end
 	end
+
+	return nil;
+end
+
+----------------------- FUNCTION : TARGET MOB  ---------------------------------
+
+function selfHeal()
+	if (GetMe():GetHpPercent() < perce_hp_heal) then -- Target is not nul
+		Command("/target Mengo")
+		Sleep(1000)
+		Command(skillHeal);
+		Sleep(3000)
+	end
 end
 
 ----------------------- SCRIPT ---------------------------------
 
-
 repeat
-		local target = targetMob(target)
-		while (target == nil and GetTarget() == nil) do -- Check if there are mob arround and we don't have one targeted 
-		--There are not mobs available, we can /sit and wait for it
-			if (not GetMe():IsSiting()) then
-			Command("/sit")
-			Sleep(1500)
-			end
-		target = targetMob();
-		end
-		if (GetMe():IsSiting()) then -- Check if we're sitting. -- We were sitting because we didn't have mobs around, we're standing up
-			Command("/stand")
-			Sleep(1500) -- Give us time to stand up!
-		end
-        Command(targetMob()); -- Target next Mob
+		
 
+	local target = targetMob()
+	if(target ~= nil){
+		Command(target); -- Target next Mob
+	}
         repeat -- Waitting for the dead of target.
 			Sleep(1500); -- Give us time to use skills!!
-			useFightSkills() -- Using skills
-			--Command("/targetnext") -- Not sure about it. :''(
-			
+			useFightSkills() -- Using skills	
         until (GetTarget() == nil or GetTarget():IsAlikeDeath()); -- Until the mob is dead or he don't have target.
 	
 		summonGivesMana() -- check if summon has to give us mana.
@@ -133,6 +129,5 @@ repeat
         
 	Sleep(1000)
 	checkSpot(location) -- Are we far away from spot? brb, comming back
-	imDeath() -- Are we dead? Logout.
 	
 until false
